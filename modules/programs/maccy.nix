b0b3596c778f8b93f2ddb1c1a357d38c6f1b4fdc
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-05-02 16:26:55 +0900

    feat(nix): add Maccy

---
+++ modules/programs/maccy.nix
@@ -0,0 +1,15 @@
+{
+  delib,
+  host,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "programs.maccy";
+
+  options = delib.singleEnableOption host.isDesktop;
+
+  home.ifEnabled.home.packages = with pkgs; [
+    maccy
+  ];
+}
