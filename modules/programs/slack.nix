13a30e09948db300c860e385f23a453678450248
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-05-03 10:40:08 +0900

    feat(nix): add Slack

---
+++ modules/programs/slack.nix
@@ -0,0 +1,15 @@
+{
+  delib,
+  host,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "programs.slack";
+
+  options = delib.singleEnableOption host.isDesktop;
+
+  home.ifEnabled.home.packages = with pkgs; [
+    slack
+  ];
+}
