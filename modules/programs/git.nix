29cc5c8113d744fb7498118d84570b248f4775d8
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-05-03 12:08:51 +0900

    feat(nix): configure Git via Home Manager

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -1,15 +1,20 @@
-{ delib, pkgs, ... }:
+{ delib, ... }:
 delib.module {
   name = "programs.git";
 
-  home.always.home = {
-    packages = with pkgs; [
-      git
-    ];
+  home.always.programs.git = {
+    enable = true;
+    ignores = [ ".DS_Store" ];
 
-    file = {
-      ".gitconfig".source = ../../dotfiles/.gitconfig;
-      ".gitignore_global".source = ../../dotfiles/.gitignore_global;
+    settings = {
+      alias.delete-merged-branch = "!f () { git checkout $1 && git pull origin $1 && git branch --merged | grep -vE 'develop|main|master' | xargs -I % git branch -d % && git fetch --all --prune;};f";
+      core.editor = "vim";
+      ghq.root = "~/src";
+
+      user = {
+        name = "Hidekazu Tanaka";
+        email = "11025+holidayworking@users.noreply.github.com";
+      };
     };
   };
 }
