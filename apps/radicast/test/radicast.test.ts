b749a31422e2570d8588a614ca5879c99be0607c
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-05-10 18:07:43 +0900

    feat: create radicast app

--- apps/radicast/test/radicast.test.ts
+++ apps/radicast/test/radicast.test.ts
@@ -2,14 +2,14 @@ import { App } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
 import { expect, test } from "vitest";
 
-import { Route53Stack } from "../lib/route53-stack.ts";
-import snapshotPlugin from "../test/snapshot-plugin.ts";
+import { RadicastStack } from "../lib/radicast-stack.ts";
+import snapshotPlugin from "./snapshot-plugin.ts";
 
 expect.addSnapshotSerializer(snapshotPlugin);
 
 test("snapshot", () => {
   const app = new App();
-  const stack = new Route53Stack(app, "Route53Stack");
+  const stack = new RadicastStack(app, "RadicastStack");
   const template = Template.fromStack(stack);
   expect(template).toMatchSnapshot();
 });
