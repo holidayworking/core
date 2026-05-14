118444c59b7aac35c689eed50f0ac95c06b3a6ef
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-05-15 08:25:46 +0900

    fix(nix): update vite-plus pnpmDeps hash

--- overlays/nix-vite-plus.nix
+++ overlays/nix-vite-plus.nix
@@ -11,7 +11,7 @@ delib.overlayModule {
       vite-plus = upstream.vite-plus.overrideAttrs (old: {
         doInstallCheck = !(prev.stdenv.hostPlatform.isAarch64 && prev.stdenv.hostPlatform.isLinux);
         pnpmDeps = old.pnpmDeps.override {
-          hash = "sha256-RPipE1v5BN0x4cNRE8KaZ1KXiTiytCteMVBXtn8FySE=";
+          hash = "sha256-d06P6MfdkkorKp3GvwMWeEIXAEB8eI0YMO8bdOZHmJk=";
         };
         nativeBuildInputs = prev.lib.subtractLists [ prev.pnpm_10 ] old.nativeBuildInputs ++ [ prev.pnpm ];
       });
