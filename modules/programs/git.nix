dfeadbae1d1b59ee8ab3e0c1b9747ab814a4a59e
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-05-31 16:12:26 +0900

    chore(git): move .claude/settings.local.json to git ignores

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -10,7 +10,10 @@ delib.module {
     {
       programs.git = {
         enable = true;
-        ignores = [ ".DS_Store" ];
+        ignores = [
+          ".DS_Store"
+          ".claude/settings.local.json"
+        ];
 
         settings = {
           alias.delete-merged-branch = "!f () { git checkout $1 && git pull origin $1 && git branch --merged | grep -vE 'develop|main|master' | xargs -I % git branch -d % && git fetch --all --prune;};f";
