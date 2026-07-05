07dd9d4331fcbbd19d46ede32f2856028e44373e
Hidekazu Tanaka <11025+holidayworking@users.noreply.github.com>
2026-07-05 09:06:09 +0900

    feat(nix): enable Linux builder

--- nix/hosts/aries/default.nix
+++ nix/hosts/aries/default.nix
@@ -5,7 +5,14 @@ delib.host {
   system = "aarch64-darwin";
   type = "desktop";
 
-  darwin.system.stateVersion = 5;
+  darwin = {
+    system.stateVersion = 5;
+
+    nix = {
+      linux-builder.enable = true;
+      settings.trusted-users = [ "hidekazu" ];
+    };
+  };
 
   home.home.stateVersion = "25.05";
 }
